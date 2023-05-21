0a4a078cd20656f07ea071949f44e9bea0548525
oluwaseun007 <preciousopal.od@gmail.com>
2023-05-21 19:15:17 +0100

    random dad jokes server is up

--- app.js
+++ app.js
@@ -2,6 +2,39 @@ const express = require('express');
 const axios = require ('axios');
 
 const app = express();
+const port = 3000;
+
+const DadJokesAPIKey = 'ac36004e64msh2f03d5e28e75ab5p1a32bfjsn9dc87404b637';
+const DadJokesAPIURL = 'https://dad-jokes.p.rapidapi.com/random/joke';
 
 
-app.get('/', )
+// Midlewara to parse JSN body
+app.use(express.json());
+
+
+// GET /jokes
+// API endponit to get a random Dad joke
+app.get('/dad-joke', async (req, res) => {
+    try {
+        const pesponse = await axios.get(DadJokesAPIURL, {
+            headers: {
+                'x-rapidapi-key': DadJokesAPIKey,
+                'x-rapidapi-host': 'dad-jokes-p.rapidapi.com',
+                'useQueryString': true,
+            },
+        });
+
+        const randomJoke = response.data.body[0].setup + ' ' + response.data.body[0].punchline;
+        res.json({ joke: randomJoke });
+    }   catch (error) {
+        console.error('Unexpected error encountered in retrieving dad joke', error);
+        res.status(500).json({error: 'Unable to retrieve dad joke'});
+    }
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
+
