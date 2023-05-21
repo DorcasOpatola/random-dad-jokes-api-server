88740dd371c98e28aade29717169b663497b48cd
oluwaseun007 <preciousopal.od@gmail.com>
2023-05-21 20:35:10 +0100

    random dad jokes

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const axios = require ('axios');
+const axios = require('axios');
 
 const app = express();
 const port = 3000;
@@ -8,24 +8,23 @@ const DadJokesAPIKey = 'ac36004e64msh2f03d5e28e75ab5p1a32bfjsn9dc87404b637';
 const DadJokesAPIURL = 'https://dad-jokes.p.rapidapi.com/random/joke';
 
 
-// Midlewara to parse JSN body
+// Midlewara to parse JSON body
 app.use(express.json());
 
 
 // GET /jokes
 // API endponit to get a random Dad joke
-app.get('/dad-joke', async (req, res) => {
+app.get('/random-joke', async (req, res) => {
     try {
-        const pesponse = await axios.get(DadJokesAPIURL, {
+        const response = await axios.get(DadJokesAPIURL, {
             headers: {
                 'x-rapidapi-key': DadJokesAPIKey,
-                'x-rapidapi-host': 'dad-jokes-p.rapidapi.com',
-                'useQueryString': true,
+                'x-rapidapi-host': 'dad-jokes.p.rapidapi.com/random/jokes',
             },
         });
 
-        const randomJoke = response.data.body[0].setup + ' ' + response.data.body[0].punchline;
-        res.json({ joke: randomJoke });
+        const randomDadJoke = response.data.body[0].setup + ' ' + response.data.body[0].punchline;
+        res.json({randomDadJoke});
     }   catch (error) {
         console.error('Unexpected error encountered in retrieving dad joke', error);
         res.status(500).json({error: 'Unable to retrieve dad joke'});
@@ -34,6 +33,7 @@ app.get('/dad-joke', async (req, res) => {
 
 
 
+// Start the server
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
 });
